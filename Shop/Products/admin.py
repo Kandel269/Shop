9fa99ea8c52158a8d99db50ad7392c8b1c1842e0
Uzytkownik <uzytkownik@kursGIT.pl>
2022-04-15 17:42:34 +0100

    Category board game

--- Shop/Products/admin.py
+++ Shop/Products/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product, Producer
+from .models import Product, Producer, Category
 
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(Producer)
+admin.site.register(Category)
--- Shop/Products/models.py
+++ Shop/Products/models.py
@@ -13,12 +13,24 @@ class Producer(models.Model):
         verbose_name = "Producer"
         verbose_name_plural = "Producers"
 
+class Category(models.Model):
+    name = models.CharField(max_length = 100)
+    describe = models.TextField(blank = True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
 class Product(models.Model):
     name = models.CharField(max_length = 100)
     describe = models.TextField(blank = True )
     price = models.DecimalField(max_digits = 12, decimal_places = 2)
 
     Producer = models.ForeignKey(Producer,on_delete = models.CASCADE, null = True)
+    Category = models.ForeignKey(Category,on_delete = models.CASCADE, null = True)
 
     def __str__(self):
         return self.name
