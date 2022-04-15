d4ee8a1bc29e6f5216bd30ef8976843fb93c1732
Uzytkownik <uzytkownik@kursGIT.pl>
2022-04-15 16:48:31 +0100

    added Producer

--- Shop/Products/models.py
+++ Shop/Products/models.py
@@ -9,3 +9,19 @@ class Product(models.Model):
     price = models.DecimalField(max_digits = 12, decimal_places = 2)
     def __str__(self):
         return self.name
+
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+class Producer(models.Model):
+    name = models.CharField(max_length = 100)
+    describe = models.TextField(blank = True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Producer"
+        verbose_name_plural = "Producers"
