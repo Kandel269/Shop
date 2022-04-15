6442ac06bd7ab6e2fa76eef40cbde9c83bbd7054
Uzytkownik <uzytkownik@kursGIT.pl>
2022-04-15 16:02:22 +0100

    Create list of board game

--- Shop/Products/models.py
+++ Shop/Products/models.py
@@ -7,3 +7,5 @@ class Product(models.Model):
     name = models.CharField(max_length = 100)
     describe = models.TextField(blank = True )
     price = models.DecimalField(max_digits = 12, decimal_places = 2)
+    def __str__(self):
+        return self.name
--- Shop/Products/views.py
+++ Shop/Products/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def index(request):
+    request02 = Product.objects.all()
+    return HttpResponse(request02)
