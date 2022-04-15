df76779806de343c3916906163d9945d1f810d20
Uzytkownik <uzytkownik@kursGIT.pl>
2022-04-15 17:05:39 +0100

    foreignkey(producer)

--- Shop/Products/models.py
+++ Shop/Products/models.py
@@ -2,19 +2,6 @@ from django.db import models
 
 
 # Create your models here.
-
-class Product(models.Model):
-    name = models.CharField(max_length = 100)
-    describe = models.TextField(blank = True )
-    price = models.DecimalField(max_digits = 12, decimal_places = 2)
-    def __str__(self):
-        return self.name
-
-
-    class Meta:
-        verbose_name = "Product"
-        verbose_name_plural = "Products"
-
 class Producer(models.Model):
     name = models.CharField(max_length = 100)
     describe = models.TextField(blank = True)
@@ -25,3 +12,19 @@ class Producer(models.Model):
     class Meta:
         verbose_name = "Producer"
         verbose_name_plural = "Producers"
+
+class Product(models.Model):
+    name = models.CharField(max_length = 100)
+    describe = models.TextField(blank = True )
+    price = models.DecimalField(max_digits = 12, decimal_places = 2)
+
+    Producer = models.ForeignKey(Producer,on_delete = models.CASCADE, null = True)
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
